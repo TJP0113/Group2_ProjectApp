3ced66f270bb8027842c70c0c3b814470e4822d3
TJP0113 <jptang0113@gmail.com>
2022-11-26 20:42:48 +0800

    2611222042

--- app/tab3/tab3.page.ts
+++ app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  Login;
 
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(){
+    
+  }
 }
